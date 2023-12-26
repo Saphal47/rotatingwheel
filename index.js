845e66d864b67f873238162351007a2d9bdc0bde
Sal <81916015+Saphal47@users.noreply.github.com>
2023-12-26 19:37:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 
-let isSpinning = false;
-
-function toggleRotation() {
-  isSpinning = !isSpinning;
-  const disc = document.getElementById('rotating-disc');
-  disc.classList.toggle('spin', isSpinning);
-  disc.classList.toggle('reversespin', !isSpinning);
-}
